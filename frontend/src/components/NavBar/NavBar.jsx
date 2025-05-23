021a314323904f1e21d13253b8c2734805ae4114
veriartnear <veriartnear@gmail.com>
2025-05-23 20:56:02 +0100

    Created Hero component, completed the hero design

---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,8 @@
+// Style
+import "./nav-bar.css";
+
+function NavBar() {
+  return <div className="underline text-black font-bold">NavBar</div>;
+}
+
+export default NavBar;
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+// Style
+import "./home.css";
+
+// Components
+import NavBar from "../../components/NavBar/NavBar";
+
+function Home() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default Home;
